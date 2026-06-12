4066a0de1b868ff3130e08755221102648ef66aa
Yueming Yuan <yym022502@gmail.com>
2026-06-12 11:46:39 -0700

    [fix] set deepseek v32 override_hf_native=True (#1330)

--- miles/utils/hf_config.py
+++ miles/utils/hf_config.py
@@ -35,6 +35,7 @@ _CONFIG_ALIASES: tuple[_HFConfigAlias, ...] = (
         base_module="transformers.models.deepseek_v3.configuration_deepseek_v3",
         base_class="DeepseekV3Config",
         compat_class_name="DeepseekV32Config",
+        override_hf_native=True,
     ),
     _HFConfigAlias(
         model_type="deepseek_v4",
